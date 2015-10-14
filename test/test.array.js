e356e0c3e1bf3db9228765f76ab67c655d7df812
Eugene Krevenets <hyzhak@ya.ru>
2015-10-15 00:42:41 +0200

    remove failed test

--- test/test.array.js
+++ test/test.array.js
@@ -6,9 +6,5 @@ describe('Array', function() {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
-
-    it('should fail test', function() {
-      assert.equal(true, false);
-    });
   });
 });
