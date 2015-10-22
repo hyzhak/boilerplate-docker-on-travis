2217cacd7555e6ffacba8cdb06a5e841db9f12ae
Eugene Krevenets <hyzhak@ya.ru>
2015-10-22 10:56:01 +0200

    use preinstalled docker-compose

--- py/test_connections.py
+++ py/test_connections.py
@@ -10,12 +10,8 @@ class TestMemcache(unittest.TestCase):
     def test_elastic_connection(self):
         print('ELASTIC_PORT_9200_TCP_ADDR')
         print(os.environ.get('ELASTIC_PORT_9200_TCP_ADDR'))
-        print('ELASTICSEARCH_PORT_9200_TCP_ADDR')
-        print(os.environ.get('ELASTICSEARCH_PORT_9200_TCP_ADDR'))
         print('ELASTIC_PORT_9200_TCP_PORT')
         print(os.environ.get('ELASTIC_PORT_9200_TCP_PORT'))
-        print('ELASTICSEARCH_PORT_9200_TCP_PORT')
-        print(os.environ.get('ELASTICSEARCH_PORT_9200_TCP_PORT'))
         es = Elasticsearch([{'host': os.environ.get('ELASTIC_PORT_9200_TCP_ADDR'),
                              'port': os.environ.get('ELASTIC_PORT_9200_TCP_PORT')}])
         es.index(index="my-index", doc_type="test-type", id=42, body={"any": "data", "timestamp": datetime.now()})
